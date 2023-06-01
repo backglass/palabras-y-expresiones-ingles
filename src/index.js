a1d8c3fcca5b79651fe3697ae22ca6a18f9fe8f6
Backglass <cocok29@gmail.com>
2023-06-01 13:51:33 +0200

    Cambiado el sistama de rutas por rutas hash

--- src/index.js
+++ src/index.js
@@ -4,37 +4,25 @@ import './index.css';
 
 import reportWebVitals from './reportWebVitals';
 
-import {
-  BrowserRouter,
-  Routes,
-  Route
-} from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import App from './App';
 import Palabras from "./components/pages/Palabras"
 import Expresiones from "./components/pages/Expresiones"
 import AñadirPalabras from './components/pages/AñadirPalabras';
 import AñadirExpresion from './components/pages/AñadirExpresion';
 
-
-
 ReactDOM.render(
-  
-    <BrowserRouter>
+  <HashRouter>
     <Routes>
       <Route path="/" element={<App />}>
         <Route path="palabras" element={<Palabras />} />
         <Route path="expresiones" element={<Expresiones />} />
         <Route path="añadir-palabra" element={<AñadirPalabras />} />
         <Route path="añadir-expresion" element={<AñadirExpresion />} />
-
       </Route>
     </Routes>
-  </BrowserRouter>,
-  
+  </HashRouter>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
