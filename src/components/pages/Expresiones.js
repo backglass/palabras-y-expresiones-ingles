dd53f20267f29f96037e05d32e3530d1687cc42f
Backglass <cocok29@gmail.com>
2023-06-07 12:43:25 +0200

    Añadido obtener el id por parámetro de url y según el valor del id crea un ExpresionCard con el id,

--- src/components/pages/Expresiones.js
+++ src/components/pages/Expresiones.js
@@ -1,12 +1,5 @@
 
 import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Button from '@mui/material/Button';
-import CameraIcon from '@mui/icons-material/PhotoCamera';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
 import CssBaseline from '@mui/material/CssBaseline';
 import Grid from '@mui/material/Grid';
 import Stack from '@mui/material/Stack';
@@ -20,7 +13,8 @@ import ExpresionCard from '../ExpresionCard/ExpresionCard';
 import Footer from '../Footer/Footer';
 import { IconButton } from '@mui/material';
 import RefreshIcon from '@mui/icons-material/Refresh';
-
+import { useParams } from 'react-router-dom';
+import { useState } from 'react';
 
 
 function Copyright() {
@@ -41,8 +35,21 @@ const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 const theme = createTheme();
 
 function Expresiones() {
-
-  
+  const { id } = useParams(); // Obtener el valor del parámetro "id"
+  console.log(id);
+  let [expresion, setExpresion] = useState(id); // Se inicializa el estado con el valor del parámetro "id"
+ 
+  // Si el parámetro es "Lección 1" se cambia a "all" para que se muestren todas las palabras
+  if (expresion === "Expresión 1") {
+    expresion = "1";
+  }
+  if (expresion === "Expresión 2") {
+    expresion = "2";
+  }
+  if (expresion === "Expresión 3") {
+    expresion = "3";
+  }
+  console.log(expresion);
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
@@ -65,7 +72,7 @@ function Expresiones() {
               color="text.primary"
               gutterBottom
             >
-              Expresiones
+              {id}
             </Typography>
   
           </Container>
@@ -75,22 +82,22 @@ function Expresiones() {
           
           <Grid container>
             <Grid item xs={12} sm={6} md={4}>
-            <ExpresionCard sx={{padding: "30px"}}/>
+            <ExpresionCard id ={"all"} sx={{padding: "30px"}}/>
             </Grid>
             <Grid item xs={12} sm={6} md={4}>
-            <ExpresionCard sx={{padding: "30px"}}/>
+            <ExpresionCard id ={"all"} sx={{padding: "30px"}}/>
             </Grid>
             <Grid item xs={12} sm={6} md={4}>
-            <ExpresionCard sx={{padding: "30px"}}/>
+            <ExpresionCard id ={"all"} sx={{padding: "30px"}}/>
             </Grid>
             <Grid item xs={12} sm={6} md={4}>
-            <ExpresionCard sx={{padding: "30px"}}/>
+            <ExpresionCard id ={"all"} sx={{padding: "30px"}}/>
             </Grid>
             <Grid item xs={12} sm={6} md={4}>
-            <ExpresionCard sx={{padding: "30px"}}/>
+            <ExpresionCard id ={"all"} sx={{padding: "30px"}}/>
             </Grid>
             <Grid item xs={12} sm={6} md={4}>
-            <ExpresionCard sx={{padding: "30px"}}/>
+            <ExpresionCard id ={"all"} sx={{padding: "30px"}}/>
             </Grid>
      
           </Grid>
