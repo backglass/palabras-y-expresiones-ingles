c51ab4d27cae2edccb9ea11b8fad0d2748b6296d
Backglass <cocok29@gmail.com>
2023-06-08 17:19:43 +0200

    añadidos 3 bonones mas

--- src/components/pages/MenuPalabras.js
+++ src/components/pages/MenuPalabras.js
@@ -12,6 +12,10 @@ function MenuPalabras() {
       <ButtonMenuApp lección={"Lección 1"} id={"1"} />
       <ButtonMenuApp lección={"Lección 2"} id={"2"} />
       <ButtonMenuApp lección={"Lección 3"} id={"3"} />
+      <ButtonMenuApp lección={"Lección 4"} id={"4"} />
+      <ButtonMenuApp lección={"Lección 5"} id={"5"} />
+      <ButtonMenuApp lección={"Lección 6"} id={"6"} />
+
       
     </div>
 
